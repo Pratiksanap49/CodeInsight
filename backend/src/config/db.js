73e49e58c7711e643928e6ea4745938cf3947d9b
Pratik_49 <pratiksanap789@gmail.com>
2026-02-10 19:46:05 +0530

    feat: Update MongoDB connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+  family: 4,
+});
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection failed", err.message);
